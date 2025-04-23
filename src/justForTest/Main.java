57d4c3c473ca802a6e812d0753f8d55fa2b765f9
YerassylTL <151902970+YerassylTL@users.noreply.github.com>
2025-04-23 22:03:55 +0500

    just

--- src/justForTest/Main.java
+++ src/justForTest/Main.java
@@ -1,15 +1,30 @@
 package justForTest;
+import java.util.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        String a = new String("Yes");
-        String b = new String("Yes");
-        System.out.println(a == b);
-        System.out.println(a.equals(b));
-        String c = "Yes";
-        System.out.println(a == c);
-        Integer j = 200;
-        Integer k = 200;
-        System.out.println(j == k);
+//        String a = new String("Yes");
+//        String b = new String("Yes");
+//        System.out.println(a == b);
+//        System.out.println(a.equals(b));
+//        String c = "Yes";
+//        System.out.println(a == c);
+//        Integer j = 200;
+//        Integer k = 200;
+//        System.out.println(j == k);
+       int a = 100234;
+       String b = Integer.toString(a);
+       String c = "";
+       for(int i = 0 ; i < b.length() ; i++){
+           if(b.charAt(i) != '0'){
+               c += b.charAt(i);
+           }
+       }
+       int out = Integer.parseInt(c);
+        System.out.println(out);
+
+
+
     }
 }
